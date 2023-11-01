33d406f9ad3f98f5d5bbc28f3670aa44b868fb18
Kadir Akbalık <55813621+akblk01@users.noreply.github.com>
2023-11-01 14:07:32 +0300

    Add files via upload

---
+++ online-retail-car-rfm-segmentation.py
@@ -0,0 +1,126 @@
+###############################################################
+# RFM ile Müşteri Segmentasyonu (Customer Segmentation with RFM)
+###############################################################
+
+# 1. İş Problemi (Business Problem)
+# 2. Veriyi Anlama (Data Understanding)
+# 3. Veri Hazırlama (Data Preparation)
+# 4. RFM Metriklerinin Hesaplanması (Calculating RFM Metrics)
+# 5. RFM Skorlarının Hesaplanması (Calculating RFM Scores)
+# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
+# 7. Tüm Sürecin Fonksiyonlaştırılması
+
+###############################################################
+# 1. İş Problemi (Business Problem)
+###############################################################
+
+# Bir e-ticaret şirketi müşterilerini segmentlere ayırıp bu segmentlere göre
+# Pazarlama stratejileri belirlemek istiyor.
+
+# Veri Seti Hikayesi
+# https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
+
+# Online Retail II isimli veri seti İngiltere merkezli online bir satış mağazasının
+# 01/12/2009 - 09/12/2011 tarihleri arasındaki satışlarını içeriyor.
+
+# Değişkenler
+#
+# InvoiceNo: Fatura numarası. Her işleme yani faturaya ait eşsiz numara. C ile başlıyorsa iptal edilen işlem.
+# StockCode: Ürün kodu. Her bir ürün için eşsiz numara.
+# Description: Ürün ismi
+# Quantity: Ürün adedi. Faturalardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
+# InvoiceDate: Fatura tarihi ve zamanı.
+# UnitPrice: Ürün fiyatı (Sterlin cinsinden)
+# CustomerID: Eşsiz müşteri numarası
+# Country: Ülke ismi. Müşterinin yaşadığı ülke.
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df_ = pd.read_excel("datasets/online_retail_II.xlsx", sheet_name="Year 2010-2011")
+df = df_.copy()
+
+###############################################################
+# Data Understanding
+###############################################################
+
+# Essiz urun sayisi nedir?
+df["Description"].nunique()
+
+# Essiz urunlerden faturalarda kaç kez gecti?
+df["Description"].value_counts().head()
+
+# Essiz urunlerden toplam kacar tane siparis verildi?
+df.groupby("Description").agg({"Quantity": "sum"}).head()
+df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
+
+# Kaçar tane essiz fatura geçti, toplam kaç adet essiz fatura var?
+df["Invoice"].nunique()
+
+# Monetary değeri icin Quantity ile Price değişkenini carpıp her bir ürünün tam fiyatını elde etmiş oluruz.
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+# Fatura başı toplam harcanan para;
+df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
+
+###############################################################
+# RFM Analysis
+###############################################################
+
+def create_rfm(dataframe, csv=False):
+    # VERIYI HAZIRLAMA
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+
+    # RFM METRIKLERININ HESAPLANMASI
+    # Son alışveriş tarihi
+    last_date = df["InvoiceDate"].max()
+
+    # 2 gün eklemek için bir timedelta nesnesi oluştur.
+    add_days = dt.timedelta(days=2)
+
+    # Tarihe timedelta ekle
+    analysis_date = last_date + add_days
+
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (analysis_date - date.max()).days,
+                                                'Invoice': lambda num: num.nunique(),
+                                                "TotalPrice": lambda price: price.sum()})
+    rfm.columns = ['recency', 'frequency', "monetary"]
+    rfm = rfm[(rfm['monetary'] > 0)]
+
+    # RFM SKORLARININ HESAPLANMASI
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+    # cltv_df skorları kategorik değere dönüştürülüp df'e eklendi
+    rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                        rfm['frequency_score'].astype(str))
+
+    # SEGMENTLERIN ISIMLENDIRILMESI
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+
+    rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+    rfm.index = rfm.index.astype(int)
+
+    if csv:
+        rfm.to_csv("rfm.csv")
+
+    return rfm
+
+rfm = create_rfm(df, True)
